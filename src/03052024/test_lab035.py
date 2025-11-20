a1c8a41dea8f04cc2302763ac76ea64036e5d9b3
UTKARSH <utkarsh.upadhyay.qa@gmail.com>
2025-11-20 16:24:27 +0530

    hi

---
+++ src/03052024/test_lab035.py
@@ -0,0 +1,34 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.actions.mouse_button import MouseButton
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def test_01_alerts():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+    driver.maximize_window()
+    button = driver.find_element(By.XPATH, "//button[@onclick='jsPrompt()']")
+    button.click()
+
+    wait = WebDriverWait(driver,10)
+    wait.until(EC.alert_is_present())
+    alert = driver.switch_to.alert
+    alert.send_keys("Utkarsh")
+    alert.accept()
+    # alert.dismiss() - means cancel
+
+    # popups -> 1- model or html popup, 2- if its alert - this how to handle
+    # wait for the model to come and click on the escape button, or close button
+    result = driver.find_element(By.ID, "result")
+    print(result.text)
+    assert "Utkarsh" in result.text
+
+    time.sleep(10)
+    driver.quit()
---
+++ src/03052024/test_lab038.py
@@ -0,0 +1,22 @@
+# JavaScript Executor
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def test_01_JS1():
+    driver = webdriver.Chrome()
+    driver.get("https://the-internet.herokuapp.com/add_remove_elements/")
+    # Javascript executor
+    button = driver.find_element(By.XPATH, "//button[@onclick='addElement()']")
+    # button.click()
+
+    # Use the Javascript code to click on this button also
+    js_ex = driver.execute_script
+    js_ex("arguments[0].click()", button)  # On button element execute this code/script(arguments[0].click())
+    js_ex("arguments[0].click()", button)
+
+    time.sleep(10)
+    driver.quit()
