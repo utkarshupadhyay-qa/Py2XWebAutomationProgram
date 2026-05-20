15e09757be7ae571056ce2454a6c8d7f4a70b8e5
UTKARSH <utkarsh.upadhyay.qa@gmail.com>
2026-05-20 20:44:16 +0530

    hi

--- src/15042024/test_lab014.py
+++ src/15042024/test_lab014.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 
 # @pytest.mark.smoke
-def test_open_vwologin():
+def test_katalon_demo():
     driver = webdriver.Chrome()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
 
